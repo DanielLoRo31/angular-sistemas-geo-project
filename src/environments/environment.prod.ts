8a5e1153df095d6d96bbaa72469979cabb52c3a8
DanielLoRo31 <daniel.lozano.rodriguez2000@gmail.com>
2021-11-19 12:07:37 -0600

    add: update maps credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,4 +9,5 @@ export const environment = {
     messagingSenderId: "996983390007",
     appId: "1:996983390007:web:9f46702f37e8f436d7e4b2"
   },
+  GOOGLE_MAPS_URL: 'AIzaSyDnY8kMwai0w66KFgVDfGoFom4BfJ-hkM0'
 };
